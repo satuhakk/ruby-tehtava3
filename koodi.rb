ea80fe0a192a950492346ff4b48fd7c9a99a864b
Satu Häkkinen <s.j.hakkinen@gmail.com>
2016-01-21 19:15:09 +0200

    Kertoma ok

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,18 @@
-# kirjoita koodi tänne
+# def asd(luku)
+#   tilanne = luku
+#   (luku-1).times do |i|
+#     whut = luku - (i+1)
+#     tilanne = tilanne * whut
+#   end
+#   return tilanne
+# end
+
+def kertoma(luku)
+  tulos = 1
+  1.upto(luku) do |i|
+    tulos = tulos * i
+  end
+  return tulos
+end
+
+kertoma(1)
